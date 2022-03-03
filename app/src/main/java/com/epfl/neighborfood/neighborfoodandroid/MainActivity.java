d39dfcc3474f707c8dbe976d2a3288e65fb0069c
khalil acheche <khalil.acheche@epfl.ch>
2022-03-03 11:21:34 +0100

    Hello world app

---
+++ app/src/main/java/com/epfl/neighborfood/neighborfoodandroid/MainActivity.java
@@ -0,0 +1,14 @@
+package com.epfl.neighborfood.neighborfoodandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
