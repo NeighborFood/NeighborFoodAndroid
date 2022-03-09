43156505fc6f0530d07f2abf76b9d3c3e8caed59
Fatnassi99 <79871958+Fatnassi99@users.noreply.github.com>
2022-03-09 21:55:39 +0100

    added google-service file

--- app/src/main/java/com/epfl/neighborfood/neighborfoodandroid/SignUpActivity.java
+++ app/src/main/java/com/epfl/neighborfood/neighborfoodandroid/SignUpActivity.java
@@ -3,11 +3,26 @@ package com.epfl.neighborfood.neighborfoodandroid;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
 public class SignUpActivity extends AppCompatActivity {
+
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
     }
 }
