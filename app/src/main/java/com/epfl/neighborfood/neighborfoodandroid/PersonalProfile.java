f971f3e871908539baba8e992a5ceba587025087
khalil acheche <khalil.acheche@epfl.ch>
2022-03-10 23:12:16 +0100

    profile editing and account page

---
+++ app/src/main/java/com/epfl/neighborfood/neighborfoodandroid/PersonalProfile.java
@@ -0,0 +1,14 @@
+package com.epfl.neighborfood.neighborfoodandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonalProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_profile);
+    }
+}
