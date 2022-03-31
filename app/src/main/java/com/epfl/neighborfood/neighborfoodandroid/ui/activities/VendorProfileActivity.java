ab347ae48e209298d851c3cc453b29125f75c738
Ali Raed Ben Mustapha <ali.benmustapha@epfl.ch>
2022-03-31 03:07:19 +0200

    Revert "added setters for TextViews"
    This reverts commit d108e58b469772aa635ad69f21dc875cddf8fb23.

--- app/src/main/java/com/epfl/neighborfood/neighborfoodandroid/ui/activities/VendorProfileActivity.java
+++ app/src/main/java/com/epfl/neighborfood/neighborfoodandroid/ui/activities/VendorProfileActivity.java
@@ -1,9 +1,9 @@
 package com.epfl.neighborfood.neighborfoodandroid.ui.activities;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,30 +15,12 @@ public class VendorProfileActivity extends AppCompatActivity implements View.OnC
     Boolean subscribed = false;
     ImageView notificationButton;
     Boolean notifyOn = false;
-
-    TextView name,rate,reviews,likes,nbrMeals,bio;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vendor_profile);
         notificationButton = findViewById(R.id.notificationId);
         subscribeButton = findViewById(R.id.SubscribeId);
-        name = findViewById(R.id.NameId);
-        setName("SDP fan");
-        reviews = findViewById(R.id.reviewNbrId);
-        setReviews(60);
-        rate = findViewById(R.id.rateId);
-        setRate("4.8");
-
-
-        nbrMeals = findViewById(R.id.nbrMealsId);
-        setNbrMeals(66);
-        likes = findViewById(R.id.LikesId);
-        setLikes(113);
-        bio = findViewById(R.id.bioId);
-        setBio("I love cooking");
-
         subscribeButton.setOnClickListener(this);
         notificationButton.setOnClickListener(this);
     }
@@ -65,29 +47,5 @@ public class VendorProfileActivity extends AppCompatActivity implements View.OnC
                     notifyOn = false;
                 }
         }
-    }
-
-    public void setName(String name) {
-        this.name.setText(name);
-    }
-
-    public void setLikes(int likes) {
-        this.likes.setText(Integer.toString(likes));
-    }
-
-    public void setBio(String bio) {
-        this.bio.setText(bio);
-    }
-
-    public void setRate(String rate) {
-        this.rate.setText(rate);
-    }
-
-    public void setReviews(int reviews) {
-        this.reviews.setText(Integer.toString(reviews)+" reviews");
-    }
-
-    public void setNbrMeals(int nbrMeals) {
-        this.nbrMeals.setText(Integer.toString(nbrMeals));
     }
 }
