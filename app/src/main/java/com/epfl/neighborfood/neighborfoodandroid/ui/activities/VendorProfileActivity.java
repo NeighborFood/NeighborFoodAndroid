989189dbfb30eb6183d52ad8ea68e9713e50bebf
Ali Raed Ben Mustapha <ali.benmustapha@epfl.ch>
2022-03-31 02:28:23 +0200

    Added Profile view of vendor
    in this activity:
    -Added important features(picture/name/bio/rating/total number of reviews /likes for vendors meals/total meals posted/ pictures of past meals)
    -added social media buttons(still need to link them)
    -Added subscribe and notification button
    Missing:
    -Tests
    -linking social media
    -linking data base
    Problems:
    past meals aren't visible on emulator (TO FIX)

---
+++ app/src/main/java/com/epfl/neighborfood/neighborfoodandroid/ui/activities/VendorProfileActivity.java
@@ -0,0 +1,51 @@
+package com.epfl.neighborfood.neighborfoodandroid.ui.activities;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.epfl.neighborfood.neighborfoodandroid.R;
+
+public class VendorProfileActivity extends AppCompatActivity implements View.OnClickListener{
+
+    ImageView subscribeButton;
+    Boolean subscribed = false;
+    ImageView notificationButton;
+    Boolean notifyOn = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vendor_profile);
+        notificationButton = findViewById(R.id.notificationId);
+        subscribeButton = findViewById(R.id.SubscribeId);
+        subscribeButton.setOnClickListener(this);
+        notificationButton.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.SubscribeId:
+                if(!subscribed){
+                    subscribeButton.setImageResource(R.drawable.full_heart);
+                    subscribed = true;
+                }
+                else{
+                    subscribeButton.setImageResource(R.drawable.empty_heart);
+                    subscribed = false;
+                }
+                break;
+            case R.id.notificationId:
+                if(!notifyOn){
+                    notificationButton.setImageResource(R.drawable.full_notif);
+                    notifyOn = true;
+                }
+                else{
+                    notificationButton.setImageResource(R.drawable.empty_notif);
+                    notifyOn = false;
+                }
+        }
+    }
+}
