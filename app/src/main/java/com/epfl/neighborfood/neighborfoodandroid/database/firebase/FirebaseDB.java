8c7782c9c0b76ba2ca7bf65a8687a820fdba1a64
Mohamed Yassine Boukhari <mohamed.boukhari@epfl.ch>
2022-04-05 12:21:46 +0200

    fixing build but tests still crash

---
+++ app/src/main/java/com/epfl/neighborfood/neighborfoodandroid/database/firebase/FirebaseDB.java
@@ -0,0 +1,69 @@
+package com.epfl.neighborfood.neighborfoodandroid.database.firebase;
+
+import android.annotation.SuppressLint;
+
+import com.epfl.neighborfood.neighborfoodandroid.database.DBData;
+import com.epfl.neighborfood.neighborfoodandroid.database.DBDataCollection;
+import com.epfl.neighborfood.neighborfoodandroid.database.DummyDatabase;
+import com.epfl.neighborfood.neighborfoodandroid.database.TentativeDB;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Map;
+
+public class FirebaseDB implements TentativeDB {
+
+    private static final String TAG = "FirebaseDB";
+    private static FirebaseDB instance;
+
+
+    private static FirebaseFirestore database;
+
+
+    public FirebaseDB() {
+        //database = FirebaseFirestore.getInstance();
+        //FirebaseAuth.getInstance();
+    }
+
+    public static  FirebaseDB getInstance() {
+        if (instance== null) {
+            instance = new FirebaseDB();
+        }
+        return instance;
+    }
+
+
+    @Override
+    public Task<DBData> fetch(String collectionPath, String documentPath) {
+        return database.collection(collectionPath).document(documentPath)
+                .get().onSuccessTask((documentSnapshot ->
+                        Tasks.forResult(new FirebaseData(documentSnapshot))
+                ));
+    }
+
+    @Override
+    public Task<Void> set(String collectionPath, String documentPath, Map<String, Object> data) {
+        return database.collection(collectionPath).document(documentPath)
+                .set(data);
+    }
+
+    @Override
+    public Task<Void> delete(String collectionPath, String documentPath) {
+        return database.collection(collectionPath).document(documentPath).delete();
+    }
+
+    @Override
+    public Task<String> add(String collectionPath, Map<String, Object> data) {
+        return database.collection(collectionPath)
+                .add(data).onSuccessTask((documentReference ->
+                        Tasks.forResult(documentReference.getId())
+                ));
+    }
+
+    @Override
+    public Task<DBDataCollection> fetchAll(String collectionPath) {
+        return null;
+    }
+}
---
+++ app/src/test/java/com/epfl/neighborfood/neighborfoodandroid/database/DatabaseFactoryTest.java
@@ -0,0 +1,50 @@
+package com.epfl.neighborfood.neighborfoodandroid.database;
+
+import static org.junit.Assert.assertEquals;
+
+import com.epfl.neighborfood.neighborfoodandroid.database.DBData;
+import com.epfl.neighborfood.neighborfoodandroid.database.DBDataCollection;
+import com.epfl.neighborfood.neighborfoodandroid.database.DatabaseFactory;
+import com.epfl.neighborfood.neighborfoodandroid.database.TentativeDB;
+import com.google.android.gms.tasks.Task;
+
+import org.junit.Test;
+
+import java.util.Map;
+
+public class DatabaseFactoryTest {
+
+    @Test
+    public void dependencyTest() {
+
+        TentativeDB dep = new TentativeDB() {
+            @Override
+            public Task<DBData> fetch(String collectionPath, String documentPath) {
+                return null;
+            }
+
+            @Override
+            public Task<Void> set(String collectionPath, String documentPath, Map<String, Object> data) {
+                return null;
+            }
+
+            @Override
+            public Task<Void> delete(String collectionPath, String documentPath) {
+                return null;
+            }
+
+            @Override
+            public Task<String> add(String collectionPath, Map<String, Object> data) {
+                return null;
+            }
+
+            @Override
+            public Task<DBDataCollection> fetchAll(String collectionPath) {
+                return null;
+            }
+        };
+
+        DatabaseFactory.setDependency(dep);
+        assertEquals(dep,DatabaseFactory.getDependency());
+    }
+}
