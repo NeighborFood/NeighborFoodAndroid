875e5cce59f649b49ae41dcb7d1a6c0ae69a8d36
Badis Machraoui <badis.machraoui@epfl.ch>
2022-04-22 14:52:49 +0100

    implemented fetching gps location
    For now the location is only displayed on the app. Later will be used to filter the meal list.

--- app/src/main/java/com/epfl/neighborfood/neighborfoodandroid/ui/activities/MainActivity.java
+++ app/src/main/java/com/epfl/neighborfood/neighborfoodandroid/ui/activities/MainActivity.java
@@ -1,13 +1,22 @@
 package com.epfl.neighborfood.neighborfoodandroid.ui.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.Manifest;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.epfl.neighborfood.neighborfoodandroid.R;
@@ -16,13 +25,18 @@ import com.epfl.neighborfood.neighborfoodandroid.ui.fragments.ConversationsFragm
 import com.epfl.neighborfood.neighborfoodandroid.ui.fragments.MealListFragment;
 import com.epfl.neighborfood.neighborfoodandroid.ui.fragments.VendorDashboardFragment;
 import com.google.android.material.navigation.NavigationBarView;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.epfl.neighborfood.MESSAGE";
     private NavigationBarView navbar;
+    private TextView lat;
+    private TextView lon;
     private Button toggleButton;
     private boolean isVendor;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,11 +47,35 @@ public class MainActivity extends AppCompatActivity {
         isVendor=false;
         toggleButton=findViewById(R.id.button2);
         toggleButton.setOnClickListener(this::toggleView);
+
+        lat = findViewById(R.id.latitude);
+        lon = findViewById(R.id.longitude);
+
+        setupLocation();
+
     }
+
 
     private void toggleView(View view) {
         isVendor=!isVendor;
         switchFragment(navbar.getMenu().findItem(navbar.getSelectedItemId()));
+    }
+
+    private void setupLocation(){
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if(ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this , new String[] {android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION},1);
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10, 1, new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location){
+                lon.setText(String.valueOf(location.getLongitude()));
+                lat.setText(String.valueOf(location.getLatitude()));
+            }
+        });
     }
 
     private boolean switchFragment(MenuItem it){
@@ -69,7 +107,5 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, fragment,null)/*.addToBackStack(null)*/.commit();
 
     }
-
-
 
 }
