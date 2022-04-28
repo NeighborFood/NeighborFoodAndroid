e4da88cb68d05abc6bacbbf95ff4e3017f5ee470
Ali Raed Ben Mustapha <ali.benmustapha@epfl.ch>
2022-04-29 00:05:44 +0200

    Refactored as suggested in Spring Break review

--- app/src/main/java/com/epfl/neighborfood/neighborfoodandroid/ui/activities/VendorProfileActivity.java
+++ app/src/main/java/com/epfl/neighborfood/neighborfoodandroid/ui/activities/VendorProfileActivity.java
@@ -1,8 +1,6 @@
 package com.epfl.neighborfood.neighborfoodandroid.ui.activities;
 
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -12,64 +10,71 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.epfl.neighborfood.neighborfoodandroid.R;
 
-public class VendorProfileActivity extends AppCompatActivity implements View.OnClickListener{
-    ImageView subscribeButton;
-    Boolean subscribed = false;
-    ImageView notificationButton;
-    Boolean notifyOn = false;
-    ImageView facebookIcon,instagramIcon, twitterIcon;
+/**
+ * Activity displays the profile of the vendor with all his details.
+ *
+ */
+public class VendorProfileActivity extends AppCompatActivity implements View.OnClickListener {
+    private ImageView subscribeButton;
+    private Boolean subscribed = false;
+    private ImageView notificationButton;
+    private Boolean notifyOn = false;
     private int heart = R.drawable.empty_heart;
     private int notif = R.drawable.empty_notif;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vendor_profile);
         notificationButton = findViewById(R.id.notificationId);
         subscribeButton = findViewById(R.id.SubscribeId);
-        facebookIcon = findViewById(R.id.facebookId);
-        instagramIcon = findViewById(R.id.instagramId);
-        twitterIcon = findViewById(R.id.TwitterId);
-        facebookIcon.setOnClickListener(this);
-        instagramIcon.setOnClickListener(this);
-        twitterIcon.setOnClickListener(this);
         subscribeButton.setOnClickListener(this);
         notificationButton.setOnClickListener(this);
+        findViewById(R.id.facebookId).setOnClickListener(this);
+        findViewById(R.id.instagramId).setOnClickListener(this);
+        findViewById(R.id.TwitterId).setOnClickListener(this);
+
     }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.SubscribeId:
-                subscribed= !subscribed;
-                heart= (subscribed)?R.drawable.full_heart:R.drawable.empty_heart;
+                subscribed = !subscribed;
+                heart = (subscribed) ? R.drawable.full_heart : R.drawable.empty_heart;
                 subscribeButton.setImageResource(heart);
                 subscribeButton.setTag(heart);
                 break;
             case R.id.notificationId:
-                notifyOn= !notifyOn;
-                notif= (notifyOn)?R.drawable.full_notif:R.drawable.empty_notif;
+                notifyOn = !notifyOn;
+                notif = (notifyOn) ? R.drawable.full_notif : R.drawable.empty_notif;
                 notificationButton.setImageResource(notif);
                 notificationButton.setTag(notif);
                 break;
-            case R.id.facebookId:{
-                String webLinkStr = "https://www.facebook.com/gordonramsay";
+            case R.id.facebookId: {
+                String webLinkStr = getString(R.string.FacebookLink) + "gordonramsay";
                 openLink(webLinkStr);
                 break;
             }
-            case R.id.instagramId:{
-                String webLinkStr = "https://www.instagram.com/gordongram";
+            case R.id.instagramId: {
+                String webLinkStr = getString(R.string.InstagramLink) + "gordongram";
                 openLink(webLinkStr);
                 break;
             }
-            case R.id.TwitterId:{
-                String webLinkStr = "https://twitter.com/GordonRamsay";
+            case R.id.TwitterId: {
+                String webLinkStr = getString(R.string.twitterLink) + "GordonRamsay";
                 openLink(webLinkStr);
                 break;
             }
 
         }
     }
-    public void openLink(String webLinkStr){
-        //TODO: try catch later
+
+    /**
+     * loads a link in browser.
+     * @param webLinkStr
+     */
+    public void openLink(String webLinkStr) {
         Uri uri = Uri.parse(webLinkStr);
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(uri);
