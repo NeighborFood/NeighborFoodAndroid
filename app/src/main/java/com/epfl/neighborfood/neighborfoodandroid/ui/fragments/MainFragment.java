d5d07b20df9f2d9835d31863c28fbee09b488999
khalil acheche <khalil.acheche@epfl.ch>
2022-06-02 01:28:50 +0200

    updated colors + modified toggle

---
+++ app/src/main/java/com/epfl/neighborfood/neighborfoodandroid/ui/fragments/MainFragment.java
@@ -0,0 +1,42 @@
+package com.epfl.neighborfood.neighborfoodandroid.ui.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.fragment.app.Fragment;
+
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+
+import com.epfl.neighborfood.neighborfoodandroid.R;
+
+public class MainFragment extends Fragment {
+    private SwitchCompat userModeSwitch;
+
+    public MainFragment() {
+        super(R.layout.fragment_main);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        userModeSwitch = getActivity().findViewById(R.id.modeSwitch);
+        userModeSwitch.setOnCheckedChangeListener(this::toggleView);
+        updateSwitchButtonText(userModeSwitch.isChecked());
+
+    }
+
+    private void toggleView(CompoundButton compoundButton, boolean b) {
+        getChildFragmentManager().beginTransaction().replace(R.id.MainFragmentContainerFragment, b ? VendorDashboardFragment.class: MealListFragment.class, null).commit();
+        updateSwitchButtonText(b);
+    }
+    private void updateSwitchButtonText(boolean state){
+        userModeSwitch.setText(state? getResources().getString(R.string.vendor_mode_title) : getResources().getString(R.string.buyer_mode_title));
+
+    }
+}
